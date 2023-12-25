42c6ac425dcf7d25409aead4152a77b15ddb2476
Laurence Durnan <ledurnan@gmail.com>
2023-12-25 14:04:44 +0000

    Update MIDDLEWARE, DEFAULT_AUTO_FIELD, TEMPLATES, django.contrib.messages

--- capone/tests/settings.py
+++ capone/tests/settings.py
@@ -11,6 +11,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.auth',
     'django.contrib.admin',
+    'django.contrib.messages',
 )
 
 SECRET_KEY = 'secretkey'
@@ -37,10 +38,33 @@ STATIC_FILE_FINDERS = (
 
 STATIC_URL = '/static/'
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-)
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+###########
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
